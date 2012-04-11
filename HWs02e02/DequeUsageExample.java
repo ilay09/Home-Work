8d924650a21f7fc2caaedcc255c7ce33c53f6252
ilay09 <ilay09@inbox.ru>
2012-04-11 12:27:14 +0400

    Update HWs02e02/DequeUsageExample.java

--- HWs02e02/DequeUsageExample.java
+++ HWs02e02/DequeUsageExample.java
@@ -1,15 +1,5 @@
 public class DequeUsageExample {
-public static void poperror(Deque x)throws DequeOutOfBoundException {
- if (x.data.length==0) throw new DequeOutOfBoundException();
-	x.popFront();
-}
-public static void main(String[] args){
-	Deque x = new Deque();
-	try {
-	poperror(x);
-	} catch (DequeOutOfBoundException e) {
-	System.out.println(e);
 
-}
-}
+public static void main(String[] args){
+	
 }
