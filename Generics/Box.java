04506a07a9d851dddd71e4f4421a6eac2cd55c9b
Usmanova Ilusa <usmanova456@mail.ru>
2012-05-18 18:18:26 +0400

    commit

---
+++ Generics/Box.java
@@ -0,0 +1,18 @@
+﻿public class Box<T> {         //Т-тип
+    private T t;
+
+    public void add(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+        integerBox.add(new Integer(10));
+        Integer someInteger = integerBox.get();
+        System.out.println(someInteger);
+    }
+}
